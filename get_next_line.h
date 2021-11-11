32faae7eeb3ed8d71c4063de4193facb0b7c203e
aposipov <a.p.osipov@ro.ru>
2021-11-11 19:21:02 +1000

    ok

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,15 @@
 /*   By: lchristi <lchristi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 16:27:38 by lchristi          #+#    #+#             */
-/*   Updated: 2021/11/04 18:12:23 by lchristi         ###   ########.fr       */
+/*   Updated: 2021/11/11 19:20:41 by lchristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#define BUFFER_SIZE = 42
 
+char *get_next_line(int fd);
 
 #endif
